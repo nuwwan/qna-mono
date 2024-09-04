18df7a0350d58fcedb8a125f756e8949b74d6e50
nuwan_rathnayaka <nuwan.cse@gmail.com>
2024-09-04 23:29:19 +0530

    user views

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateAPIView
 
-# Create your views here.
+
+class CreateProfile:
+    pass
+
+
+class ProfileDetail(RetrieveUpdateAPIView):
+    pass
+
+
+"""
+Subject Views
+"""
+
+
+class CreateSubject:
+    pass
+
+
+class RemoveSubject:
+    pass
+
+
+class GetSubjects:
+    pass
+
+
+"""
+Tag Views
+"""
+
+
+class CreateTag:
+    pass
+
+
+class RemoveTag:
+    pass
+
+
+class GetTags:
+    pass
